8c213ceeed48b93ef6ae427443bc24997badfd8b
Patryk Wojewoda <wojewodix@gmail.com>
2024-11-24 19:26:54 +0100

    Added a TextBox for search functionality and applied styles to it. Added RadioButtons to the sidebar according to the previously created design. Added a top bar. Created a new HomeView (XAML) and corresponding HomeViewModel. Configured a DataTemplate in App.xaml to bind HomeViewModel with HomeView. Implemented CurrentView handling in MainViewModel to enable dynamic view display. Made changes to MainWindow.xaml to display HomeView in ContentControl.

---
+++ realEstateDevelopment/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,29 @@
+﻿using realEstateDevelopment.Core;
+
+namespace realEstateDevelopment.MVVM.ViewModel
+{
+    public class MainViewModel : ObservableObject
+    {
+        public HomeViewModel HomeVM { get; set; }
+
+        private object _currentView;
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            // Tworzenie instancji HomeViewModel
+            HomeVM = new HomeViewModel();
+
+            // Ustawienie CurrentView na HomeVM
+            CurrentView = HomeVM;
+        }
+    }
+}
