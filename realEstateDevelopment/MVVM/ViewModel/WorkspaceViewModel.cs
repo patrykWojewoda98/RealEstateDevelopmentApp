b65cfc705b913be3d947a8cf739ef19bbb8809c7
Patryk Wojewoda <wojewodix@gmail.com>
2024-11-26 08:18:27 +0100

    Added the BaseCommand and WorkspaceViewModel classes:
    BaseCommand implements the ICommand interface, providing a mechanism for defining command behavior with custom execute and can-execute logic.
    WorkspaceViewModel serves as a base class for view models, offering a CloseCommand and an event for closing requests, enabling the closing functionality in derived view models.

---
+++ realEstateDevelopment/MVVM/ViewModel/WorkspaceViewModel.cs
@@ -0,0 +1,46 @@
+﻿using MVVMFirma.Helper;
+using MVVMFirma.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace realEstateDevelopment.MVVM.ViewModel
+{
+    public abstract class WorkspaceViewModel : BaseViewModel
+    {
+        #region Fields
+        private BaseCommand _CloseCommand;
+        #endregion
+
+        #region Construktor
+        public WorkspaceViewModel()
+        {
+            
+        }
+        #endregion
+        #region CloseCommand
+        public ICommand CloseCommand
+        {
+            get
+            {
+                if (_CloseCommand == null)
+                    _CloseCommand = new BaseCommand(() => this.OnRequestClose());
+                return _CloseCommand;
+            }
+        }
+        #endregion
+
+        #region RequestClose [event]
+        public event EventHandler RequestClose;
+        protected void OnRequestClose()
+        {
+            EventHandler handler = this.RequestClose;
+            if (handler != null)
+                handler(this, EventArgs.Empty);
+        }
+        #endregion
+    }
+}
