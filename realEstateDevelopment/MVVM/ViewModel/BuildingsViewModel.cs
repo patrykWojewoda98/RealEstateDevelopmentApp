cf825b86ec3f60ccaeec5b030e7494a16be9706e
Patryk Wojewoda <wojewodix@gmail.com>
2024-11-26 09:38:58 +0100

    Added LoadAllViewModel base class and implemented database record fetching logic in BuildingsViewModel

--- realEstateDevelopment/MVVM/ViewModel/BuildingsViewModel.cs
+++ realEstateDevelopment/MVVM/ViewModel/BuildingsViewModel.cs
@@ -1,12 +1,28 @@
-﻿using System;
+﻿using realEstateDevelopment.MVVM.Model.Entities;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace realEstateDevelopment.MVVM.ViewModel
 {
-    public class BuildingsViewModel
+    public class BuildingsViewModel : LoadAllViewModel<Buildings>
     {
+        #region Constructor
+        public BuildingsViewModel()
+            :base() 
+        {
+            base.DisplayName = "Budynki";
+        }
+        #endregion
+
+        #region Helpers
+        public override void load()
+        {
+            List = new ObservableCollection<Buildings>(realEstateEntities.Buildings.ToList());
+        }
+        #endregion
     }
 }
---
+++ realEstateDevelopment/MVVM/ViewModel/LoadAllViewModel.cs
@@ -0,0 +1,56 @@
+﻿using MVVMFirma.Helper;
+using realEstateDevelopment.MVVM.Model.Entities;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+namespace realEstateDevelopment.MVVM.ViewModel
+{
+    public abstract class LoadAllViewModel<T> : WorkspaceViewModel 
+    {
+        #region Fields
+        protected readonly RealEstateEntities realEstateEntities;
+        private BaseCommand _LoadCommand;
+        private ObservableCollection<T> _List;
+        #endregion
+
+        #region Properties
+        public ICommand LoadCommand
+        {
+            get
+            {
+                if (_LoadCommand == null)
+                    _LoadCommand = new BaseCommand(() => load());
+                return _LoadCommand;
+            }
+        }
+
+        public ObservableCollection<T> List
+        {
+            get
+            {
+                if (_List == null)
+                    load();
+                return _List;
+            }
+            set
+            {
+                _List = value;
+                OnPropertyChanged(() => List);
+            }
+        }
+            #endregion
+
+            #region Constructor
+            public LoadAllViewModel()
+            {
+                realEstateEntities = new RealEstateEntities();
+            }
+        #endregion
+
+        #region Helpers
+        public abstract void load();
+        #endregion
+    }
+
+}
+
