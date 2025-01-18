017340382fa4f3ce2154addfe8bb7c0c3b92af45
Patryk Wojewoda <wojewodix@gmail.com>
2025-01-18 13:32:36 +0100

    Added the remaining modal views for deleting elements.

---
+++ realEstateDevelopment/MVVM/View/Modals/DeleteExpenseModalView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace realEstateDevelopment.MVVM.View.Modals
+{
+    /// <summary>
+    /// Interaction logic for DeleteExpenseModalView.xaml
+    /// </summary>
+    public partial class DeleteExpenseModalView : Window
+    {
+        public DeleteExpenseModalView()
+        {
+            InitializeComponent();
+        }
+    }
+}
